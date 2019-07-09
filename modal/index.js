49fe29340c68f5b7f40bb2ae6d6d486d373e2472
Eleasah Halsmer <eleasahjh@gmail.com>
2019-07-09 16:50:23 -0600

    wip: modal component

--- modal/index.js
+++ modal/index.js
@@ -3,8 +3,32 @@ class Modal {
   // You may create additional Classes if you feel it improves readability & reuse.
   constructor(toggleElement) {
     console.log("Modal.constructor toggleElement:", toggleElement); //
+    this.toggleElement = toggleElement;
+
+    // Get the target's ID:
+    this.target = this.toggleElement.dataset.target;
+    // console.log(this.target);
+
+    // Get the target by ID:
+    this.modal = document.querySelector(`${this.target}`);
+
+    // Add event listener to button, toggleElement:
+    this.toggleElement.addEventListener('click', this.clicked.bind(this));
+
     // Hint: Get any needed data attributes from the `toggleElement`
     // Remember your "this." syntax
+  }
+  clicked(){
+    // alert('clicked?!');
+    this.modal.style="display: block; padding-right: 16px;"
+    const outsideModal = document.querySelector(`:not(${this.target})`)
+    outsideModal.style="border: 1px solid red";
+    outsideModal.addEventListener('click', function clickOut(event){
+      console.log('click');
+      // const modalAgain = document.querySelector(`${this.target}`)
+      document.querySelector(`${this.target}`).style="display: none;"
+      event.stopPropagation();
+    });
   }
 }
 
@@ -13,3 +37,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   modalToggles.forEach(button => new Modal(button));
 });
+
+
+//           style="display: block; padding-right: 16px;"
